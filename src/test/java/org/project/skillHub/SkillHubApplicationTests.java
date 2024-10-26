3dedc08a5bd68fabf7339007e29a48687df0bef6
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-10-26 12:22:39 +0200

    Hello World, this is SkillHub!!

---
+++ src/test/java/org/project/skillHub/SkillHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.skillHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
